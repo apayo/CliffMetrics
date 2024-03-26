a4c0e7ebad00fde2b21f93c1c868d8a303d26860
Andres <payo.andres@gmail.com>
2024-03-26 14:51:08 +0000

    Delete src/CMakeFiles directory
    No need to be added to repository

--- src/CMakeFiles/cliff.dir/2di_point.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/cliff.dir/2di_point.cpp.o: \
- /home/agarcia/Projects/newCliffMetric/src/2di_point.cpp \
- /usr/include/stdc-predef.h \
- /home/agarcia/Projects/newCliffMetric/src/2di_point.h
